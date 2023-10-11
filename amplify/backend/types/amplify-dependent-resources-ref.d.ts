cee45bb7959726d8abc37ba4180c0b30308eb7e1
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2023-10-11 16:13:42 +1100

    Add Amplify's default auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "personalwebsite4e025ca8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
